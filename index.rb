ebf8ea74599ce2592d8c418f5bcd3af14d5d2462
Vlad <vlad.nykonenko@gmail.com>
2012-04-25 23:39:47 +0300

    Simple gallery

---
+++ index.rb
@@ -0,0 +1,40 @@
+require 'sinatra'
+require 'haml'
+require 'bson'
+require 'mongoid'
+require 'carrierwave'
+require 'carrierwave/mongoid'
+
+configure do
+  Mongoid.configure do |config|
+    name = "app"
+    config.master = Mongo::Connection.new.db(name)
+    config.persist_in_safe_mode = false
+  end
+end
+
+class ImageUploader < CarrierWave::Uploader::Base
+  storage :file
+end
+
+class Image
+  include Mongoid::Document
+  mount_uploader :image, ImageUploader, type: String
+  field :title, type: String
+end
+
+get '/' do
+  @images = Image.all
+  haml :'index'
+end
+
+post '/' do
+  @image = Image.new(:title => params[:title], :image => params[:image])
+  @image.save
+  redirect '/'
+end
+
+get '/images/:id' do
+  @image = Image.find(params[:id])
+  haml :'show'
+end
