7f09a6ab5386371a3198c172db6007b7671b0d66
Vlad <vlad.nykonenko@gmail.com>
2012-04-26 10:23:56 +0300

    Add stylesheet, versions and unique filename for images

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-#Simle Gallery#
+#Simple Gallery#
 
 Simple gallery on Sintra with using Haml, Carrierwave and MongoDB. It was maken by example http://habrahabr.ru/post/139423/ for futher changes and experiments.
+
+Stylesheet file(s) in .sass format, for compilation to .css use
+    sass stylesheets/index.sass:public/index.css
--- index.rb
+++ index.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
+require 'shotgun'
 require 'haml'
+require 'sass'
 require 'bson'
 require 'mongoid'
 require 'carrierwave'
@@ -14,13 +16,59 @@ configure do
 end
 
 class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
   storage :file
+  
+  def filename
+     "#{secure_token(10)}.#{file.extension}" if original_filename.present?
+  end
+  
+  version :thumb_gray do
+    process :resize_to_fill => [200, 200]
+    process :convert_to_grayscale
+  end
+  
+  version :thumb do
+    process :resize_to_fill => [200, 200]
+    process :merge
+  end
+  
+  def convert_to_grayscale
+    manipulate! do |img|
+      img.colorspace("Gray")
+      img.brightness_contrast("+15x0")
+      img = yield(img) if block_given?
+      img
+    end
+  end
+
+  def merge
+    manipulate! do |img|
+      img.combine_options do |cmd|
+        cmd.gravity "north"
+        cmd.extent "200x400"
+      end
+
+      img = img.composite(::MiniMagick::Image.open(model.image.thumb_gray.current_path, "jpg")) do |c|
+        c.gravity "south"
+      end
+      img = yield(img) if block_given?
+      img
+    end
+  end
+  
+  protected
+  
+  def secure_token(length = 16)
+    model.image_secure_token ||= SecureRandom.hex(length / 2)
+  end
 end
 
 class Image
   include Mongoid::Document
   mount_uploader :image, ImageUploader, type: String
   field :title, type: String
+  field :image_secure_token, type: String
 end
 
 get '/' do
@@ -34,7 +82,18 @@ post '/' do
   redirect '/'
 end
 
+get '/recreate' do
+  Image.all.each do |image|
+    image.image.recreate_versions!
+  end
+end
+
 get '/images/:id' do
   @image = Image.find(params[:id])
   haml :show
+end
+
+not_found do
+  status 404
+  "Something is missing! We try to find it..."
 end
